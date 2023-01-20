381c37f807044c1ed18dc0c093f3ed7932106b68
VladRuden7 <96748263+VladRuden7@users.noreply.github.com>
2023-01-20 10:28:42 +0200

    test
    test

---
+++ Models/Cinema.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCProject.Models{
+
+    public class Cinema
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Logo { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        //Relationships
+        public List<Movie> Movies { get; set; }
+
+    }
+}
